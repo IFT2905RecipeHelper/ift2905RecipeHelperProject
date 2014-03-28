864270d5a86f9d684e2f8b0463cf661742afff73
Ouattara Abdoul Yousseff <ouattyouss@live.com>
2014-03-28 00:45:38 -0400

    Create KraftAPI.java
    Classe pour l'API. C'est juste la structure, on doit la completer.

---
+++ KraftAPI.java
@@ -0,0 +1,75 @@
+package com.example.recipehelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.graphics.drawable.Drawable;
+
+
+public class KraftAPI {
+	
+	String ingredient;
+	Double quantite;
+	Drawable icone;
+	
+	String erreur;
+	
+	KraftAPI() {
+		
+		erreur = null;
+		
+		String url = "       " ;
+		
+		try {
+
+		} catch (ClientProtocolException e) {
+			erreur = "Erreur HTTP (protocole) :"+e.getMessage();
+		} catch (IOException e) {
+			erreur = "Erreur HTTP (IO) :"+e.getMessage();
+		} catch (ParseException e) {
+			erreur = "Erreur JSON (parse) :"+e.getMessage();
+		} catch (JSONException e) {
+			erreur = "Erreur JSON :"+e.getMessage();
+		}
+	}
+	
+	
+	
+	/*
+	 * Méthode utilitaire qui permet de rapidement
+	 * charger et obtenir une page web depuis
+	 * l'internet.
+	 * 
+	 */
+	private HttpEntity getHttp(String url) throws ClientProtocolException, IOException {
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet http = new HttpGet(url);
+		HttpResponse response = httpClient.execute(http);
+		return response.getEntity();    		
+	}
+		
+	/*
+	 * Méthode utilitaire qui permet
+	 * d'obtenir une image depuis une URL.
+	 * 
+	 */
+	private Drawable loadHttpImage(String url) throws ClientProtocolException, IOException {
+		InputStream is = getHttp(url).getContent();
+		Drawable d = Drawable.createFromStream(is, "src");
+		return d;
+	}
+
+}
