52e346f673cca436cecb57fd914621bdc60d7354
Julien Cloutier-Brassard <cloutij@iro.umontreal.ca>
2014-04-07 15:52:14 -0400

    Updated DB, trying to fix .gitignore

--- src/com/ift2905/recipehelper/RecipeDatabaseHelper.java
+++ src/com/ift2905/recipehelper/RecipeDatabaseHelper.java
@@ -6,17 +6,36 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class RecipeDatabaseHelper extends SQLiteOpenHelper {
 
-	static final String[] tableNames = {"shop_list","history","favorites"};
+	static final String shopListTableName = "shop_list";
+	static final String[] tableNames = {"history","favorites"};
 	static final String[] shopListColumns = {"ingredient","from_recipe","checked"};
 	static final String[] histOrFavColumns = {"recipe_id", "recipe_name", "cooking_time", "servings", "rating"};
 	Context context;
 	public RecipeDatabaseHelper(Context context) {
-		super(context, "meteo.db", null, 1);
+		super(context, "recipe.db", null, 1);
 		this.context=context;
 	}
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
+		
+		String createShopListTable = "create table" + shopListTableName + " ("
+				+ shopListColumns[0] + " text primary_key, " 
+				+ shopListColumns[1] + " text, "
+				+ shopListColumns[2] + " text)";
+		
+		db.execSQL(createShopListTable);
+		
+		for (String name: tableNames){
+			String createTable = "create table" + name + " ("
+					+ histOrFavColumns[0] + " text primary_key, "
+					+ histOrFavColumns[1] + " text, "
+					+ histOrFavColumns[2] + " text, "
+					+ histOrFavColumns[3] + " text, "
+					+ histOrFavColumns[4] + " integer)";
+			
+			db.execSQL(createTable);
+		}
 
 	}
 
