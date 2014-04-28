ee52f3f72a143a59d7aa016f24a859273212052e
Ouattara Abdoul Yousseff <ouattyouss@live.com>
2014-04-28 17:00:22 -0400

    Update RecipeDatabaseHelper.java

--- src/com/ift2905/recipehelper/RecipeDatabaseHelper.java
+++ src/com/ift2905/recipehelper/RecipeDatabaseHelper.java
@@ -9,12 +9,15 @@ public class RecipeDatabaseHelper extends SQLiteOpenHelper {
 
 	static final String[] tableNames = {"shop_list", "history","favorites"};
 	static final String[] shopListColumns = {"_id", "ingredient","from_recipe"};
-	static final String[] histOrFavColumns = {"_id", "recipe_name", "cooking_time", "servings", "rating"};
+	static final String[] historyColumns = {"_id", "recipe_name", "cooking_time", "servings", "rating"};
+	static final String[] favoriColumns = {"_id", "recipe_name", "cooking_time", "servings", "rating"};
+	
 	Context context;
 	public RecipeDatabaseHelper(Context context) {
 		super(context, "recipe.db", null, 1);
 		this.context=context;
 	}
+	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		String createShopListTable = "create table " + tableNames[0] + " ("
@@ -24,16 +27,23 @@ public class RecipeDatabaseHelper extends SQLiteOpenHelper {
 		
 		db.execSQL(createShopListTable);
 		
-		for (int i = 1; i < tableNames.length; i++){
-			String createTable = "create table " + tableNames[i] + " ("
-					+ histOrFavColumns[0] + " integer primary key, "
-					+ histOrFavColumns[1] + " text, "
-					+ histOrFavColumns[2] + " text, "
-					+ histOrFavColumns[3] + " text, "
-					+ histOrFavColumns[4] + " text)";
+		String historyTable = "create table " + tableNames[1] + " ("
+				+ historyColumns[0] + " integer primary key, "
+				+ historyColumns[1] + " text, "
+				+ historyColumns[2] + " text, "
+				+ historyColumns[3] + " text, "
+				+ historyColumns[4] + " text)";
 			
-			db.execSQL(createTable);
-		}
+		db.execSQL(historyTable);
+			
+		String favoriTable = "create table " + tableNames[2] + " ("
+				+ favoriColumns[0] + " integer primary key, "
+				+ favoriColumns[1] + " text, "
+				+ favoriColumns[2] + " text, "
+				+ favoriColumns[3] + " text, "
+				+ favoriColumns[4] + " text)";
+			
+		db.execSQL(favoriTable);
 	}
 
 	@Override
